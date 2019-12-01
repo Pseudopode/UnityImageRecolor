9c774e0b456e7a9d5f328c6ad344f107489f691a
Jean-Philippe Deblonde <jean.philippe.deblonde@gmail.com>
2019-12-01 23:33:21 +0100

    First Commit

---
+++ Assets/Materials/Recolor.shader
@@ -0,0 +1,100 @@
+﻿Shader "Unlit/Recolor"
+{
+   Properties {
+      _MainTex ("Texture Image", 2D) = "white" {}
+      _MaskTex ("Mask Image", 2D) = "white" {}
+      _NewColor("New color for recolorization", Color) = (0.5,0.5,0.5,1)
+   }
+   SubShader {
+      Pass {	
+         CGPROGRAM
+ 
+         #pragma vertex vert  
+         #pragma fragment frag 
+ 
+         uniform sampler2D _MainTex;	
+         uniform sampler2D _MaskTex;	
+
+         uniform float4 _NewColor;
+ 
+         struct vertexInput {
+            float4 vertex : POSITION;
+            float4 texcoord : TEXCOORD0;
+         };
+         struct vertexOutput {
+            float4 pos : SV_POSITION;
+            float4 tex : TEXCOORD0;
+         };
+ 
+         vertexOutput vert(vertexInput input) 
+         {
+            vertexOutput output;
+ 
+            output.tex = input.texcoord;
+            output.pos = UnityObjectToClipPos(input.vertex);
+            return output;
+         }
+
+         float4 frag(vertexOutput input) : COLOR
+         {
+            //return _NewColor;
+            //return tex2D(_MainTex, input.tex.xy);	
+            float4 inputColor = tex2D(_MainTex, input.tex.xy);
+            float4 maskVec = tex2D(_MaskTex, input.tex.xy);
+
+            /*float r = inputColor.x;
+            float g = inputColor.y;
+            float b = inputColor.z;*/
+            float mask = maskVec.x;
+
+            float grayVal = (inputColor.x + inputColor.y + inputColor.z)/3.0;
+
+            float4 grayVec = float4(grayVal, grayVal, grayVal, 1.0);
+
+            float beautyMinusProductR = inputColor.x * (1.0 - mask);
+            float beautyMinusProductG = inputColor.y * (1.0 - mask);
+            float beautyMinusProductB = inputColor.z * (1.0 - mask);
+
+            float4 beautyMinusProduct = float4(beautyMinusProductR,beautyMinusProductG,beautyMinusProductB,1.0);
+
+            float productGrayR = grayVal * mask;
+            float productGrayG = productGrayR;
+            float productGrayB = productGrayR;
+
+            float4 productGray = float4(productGrayR, productGrayG, productGrayB, 1.0);
+
+            
+            //return maskVec;
+
+            float productRGBR = productGrayR * _NewColor.x;
+            float productRGBG = productGrayG * _NewColor.y;
+            float productRGBB = productGrayB * _NewColor.z;
+
+            float4 productRGB = float4(productRGBR,productRGBG,productRGBB,1.0);
+
+            float finalR = beautyMinusProductR 
+                            + productRGBR
+                            ;
+            float finalG = beautyMinusProductG
+            + productRGBG
+                            ;
+            float finalB = beautyMinusProductB
+            + productRGBB
+                            ;
+
+            float4 finalRGB = beautyMinusProduct + productRGB;
+                
+            //float4 finalColor = float4(finalR, finalG, finalB, 1.0);
+
+            //gl_FragColor = vec4(inputRGB_r,inputRGB_g,inputRGB_b,1.0) ;
+            //gl_FragColor = float4(inputRGB_r,0.0,0.0,1.0) ;
+            return float4(finalR,finalG,finalB,1.0) ;
+            //gl_FragColor = finalColor;
+            //return finalRGB;
+         }
+ 
+         ENDCG
+      }
+   }
+   Fallback "Unlit/Texture"
+}
---
+++ Assets/Scripts/FileManager.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+using System.IO;
+
+using SFB;
+
+
+public class FileManager : MonoBehaviour
+{
+    private Texture2D openedImage;
+    private Texture2D maskImage;
+
+    private string savedImageFilepath = "";
+
+    public Texture2D exampleImage;
+    public Texture2D exampleMask;
+
+    public RenderTexture RTT;
+    public Camera RTTCam;
+
+    public RawImage imageCanvas;
+    public ColorPicker colorPicker;
+    public Material recolorMat;
+
+    public GameObject[] canvasElements;
+
+    private Color pickedColor = Color.white;
+    private bool colorPickerVisible = false;
+
+    void OnAwake()
+    {
+        float width = openedImage.width;
+        float height = openedImage.height;
+        float screenWidth = Screen.width;
+        imageCanvas.GetComponent<RectTransform>().sizeDelta = new Vector2( screenWidth, height*width/screenWidth);
+
+        savedImageFilepath = Application.dataPath + "/saveImage.png";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        savedImageFilepath = Application.dataPath + "/saveImage.png";
+        
+        colorPicker.onValueChanged.AddListener(color =>
+		{
+
+			//renderer.material.color = color;
+            //Debug.Log(color.r + " - " + color.g + " - " + color.b);
+            recolorMat.SetColor("_NewColor", color);
+		});
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Texture2D LoadPNG(string filePath) {
+     
+         Texture2D tex = null;
+         byte[] fileData;
+		  bool isLoaded = false;
+     
+         if (File.Exists(filePath))     {
+             fileData = File.ReadAllBytes(filePath);
+             tex = new Texture2D(2, 2);
+             isLoaded = tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+         }
+		 //Debug.Log(filePath + " is loaded: " + isLoaded);
+         return tex;
+     }
+
+    private Texture2D LoadMask(string beautyImgPath)
+    {
+        string textureFileName = Path.GetFileNameWithoutExtension(beautyImgPath);
+        string textureFileNameExtension = Path.GetExtension(beautyImgPath);
+	    string directoryPath = Path.GetDirectoryName(beautyImgPath);
+
+        string maskPath = directoryPath + "/" + textureFileName + "_mask" + textureFileNameExtension;
+
+        Debug.Log("maskPath: " + maskPath);
+
+        return LoadPNG(maskPath);
+    } 
+
+    public void pickExample()
+    {
+        openedImage = exampleImage;
+        maskImage = exampleMask;
+
+        setImageinGUI();
+        setImageinMaterial();
+    }
+
+    public void openImage()
+    {
+        // Open file
+        //var paths = StandaloneFileBrowser.OpenFilePanel("Open File", "", "", false);
+
+        // Open file async
+       // StandaloneFileBrowser.OpenFilePanelAsync("Open File", "", "", false, (string[] paths) => {  });
+
+        // Open file with filter
+        var extensions = new [] {
+            new ExtensionFilter("Image Files", "png", "jpg", "jpeg" ),
+            /*new ExtensionFilter("Sound Files", "mp3", "wav" ),
+            new ExtensionFilter("All Files", "*" ),*/
+        };
+        var paths = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, true);
+
+        openedImage = LoadPNG(paths[0]);
+        maskImage = LoadMask(paths[0]);
+
+
+        setImageinGUI();
+        setImageinMaterial();
+
+    }
+
+    private void setImageinGUI()
+    {
+        imageCanvas.texture = openedImage;
+        /*imageCanvas.width = openedImage.width;
+        imageCanvas.height = openedImage.height;*/
+        float width = openedImage.width;
+        float height = openedImage.height;
+        float screenWidth = Screen.width;
+        imageCanvas.GetComponent<RectTransform>().sizeDelta = new Vector2( screenWidth, height*width/screenWidth);
+
+        RTT = new RenderTexture( (int) width, (int) height, 24 );
+        RTTCam.targetTexture = RTT;
+
+
+    }
+
+    private void setImageinMaterial()
+    {
+        recolorMat.SetTexture("_MainTex", openedImage);
+        recolorMat.SetTexture("_MaskTex", maskImage);
+    }
+
+    public void showColorPicker()
+    {
+        if(colorPickerVisible == false)
+        {
+            colorPicker.gameObject.SetActive(true);
+            colorPickerVisible = true;
+        }else
+        {
+            colorPicker.gameObject.SetActive(false);
+            colorPickerVisible = false;
+        }
+    }
+
+    /*public void recolorImage(Color color)
+    {
+        recolorMat.SetTexture("_NewColor", maskImage);
+    }*/
+
+    //IEnumerator takeScreen(GameObject canvas)
+    IEnumerator takeScreen()
+    {
+        // We should only read the screen buffer after rendering is complete
+        yield return new WaitForSeconds(0.25f);
+        ScreenCapture.CaptureScreenshot(savedImageFilepath);
+        yield return new WaitForSeconds(0.25f);
+        for(int i = 0; i < canvasElements.Length; i++)
+        {
+            canvasElements[i].SetActive(true);
+        }
+    }
+
+    public void saveImage()
+    {
+        /*Debug.Log("savedImageFilepath: " + savedImageFilepath);
+        Texture2D rawImageTexture = (Texture2D)imageCanvas.texture;
+        SaveTextureToFile(rawImageTexture, savedImageFilepath);*/
+        for(int i = 0; i < canvasElements.Length; i++)
+        {
+            canvasElements[i].SetActive(false);
+        }
+        StartCoroutine(takeScreen());
+        //ScreenCapture.CaptureScreenshot(savedImageFilepath);
+        //canvas.SetActive(true);
+    }
+
+    private void SaveTextureToFile (Texture2D texture, string filename) 
+    { 
+        System.IO.File.WriteAllBytes (filename, texture.EncodeToPNG());
+    }    
+}
